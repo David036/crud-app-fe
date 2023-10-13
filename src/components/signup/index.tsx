5452befe6235c2042f9636ec2c07b5f15de497af
David036 <hovhannisyandav@icloud.com>
2023-10-13 11:16:43 +0400

    repeat password functionality

--- src/components/signup/index.tsx
+++ src/components/signup/index.tsx
@@ -7,18 +7,21 @@ import { AuthContext } from "../../context/auth/context";
 export default function SignupPage() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [repeatPassword, setRepeatPassword] = useState<string>("");
 
   const navigate = useNavigate();
   const { setIsAuth } = useContext(AuthContext);
 
   const handleSignup = async () => {
-    const registeredUser = await signup(email, password);
-    if (registeredUser) {
-      const isLoggedIn = await login(email, password);
-      if (isLoggedIn?.data.token) {
-        localStorage.setItem("accessToken", isLoggedIn?.data.token);
-        navigate("/");
-        setIsAuth(true);
+    if (repeatPassword === password && password) {
+      const registeredUser = await signup(email, password);
+      if (registeredUser) {
+        const isLoggedIn = await login(email, password);
+        if (isLoggedIn?.data.token) {
+          localStorage.setItem("accessToken", isLoggedIn?.data.token);
+          navigate("/");
+          setIsAuth(true);
+        }
       }
     }
   };
@@ -26,27 +29,31 @@ export default function SignupPage() {
   return (
     <div className={styles.signupSection}>
       <div className={styles.signupContainer}>
-      <h1>Sign up</h1>
-      <p>Email</p>
-      <input
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-        type="email"
-      />
-      <p>Password</p>
-      <input
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-        type="password"
-      />
-      <p>Repeat password</p>
-      <input type="password" />
-      <button className={styles.signupBtn} onClick={handleSignup}>
-        Sign up
-      </button>
+        <h1>Sign up</h1>
+        <p>Email</p>
+        <input
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          type="email"
+        />
+        <p>Password</p>
+        <input
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          type="password"
+        />
+        <p>Repeat password</p>
+        <input
+          onChange={(e) => setRepeatPassword(e.target.value)}
+          type="password"
+        />
+        <button className={styles.signupBtn} onClick={handleSignup}>
+          Sign up
+        </button>
+        <button onClick={() => navigate("/login")}>Login page</button>
+      </div>
     </div>
-  </div>
   );
 }
